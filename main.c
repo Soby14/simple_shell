9fb0703e85ecad7de4d454a09453f37405865e93
Soby14 <sobyria@gmail.com>
2023-10-20 05:20:15 -0700

    simple_shell

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * main - Entry point for the shell, the core of the project.
+ *
+ *Function serves as the entry point for the shell project and is responsible
+ * for initializing the shell, reading and processing user commands, and manage
+ * the execution of those commands.
+ *
+ * Return: An integer indicating the exit status of the shell.
+ */
+int main(void)
+{
+	char *buff = NULL, **args;
+	size_t read_size = 0;
+	ssize_t buff_size = 0;
+	int exit_stats = 0;
+
+	while (1)
+	{
+		if (isatty(0))
+			printf("hsh$ ");
+		buff_size = getline(&buff, &read_size, stdin);
+		if (buff_size == -1 || _strncmp("exit\n", buff) == 0)
+		{
+			free(buff);
+			break;
+		}
+		buff[buff_size - 1] = '\0';
+		if (_strncmp("env", buff) == 0)
+		{
+			_environ();
+			continue;
+		}
+		if (empt_line(buff) == 1)
+		{
+			exit_stats = 0;
+			continue;
+		}
+		args = _split(buff, " ");
+		args[0] = search_path(args[0]);
+		if (args[0] != NULL)
+			exit_stats = execute(args);
+		else
+			perror(Error");
+		free(args);
+	}
+	return (exit_stats);
+}
---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*LIBRARIES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/*PROTOTYPES*/
+int _strnlen(char *s);
+int execute(char **args);
+char *_strncpy(char *dest, char *src);
+void _env(void);
+char *_strncat(char *dest, char *src);
+int _strncmp(char *s1, char *s2);
+char *search_path(char *command);
+char *_getenviron(char *env_var);
+int empt_line(char *buff);
+void *_calloc(unsigned int nmember, unsigned int size);
+char **_splits(char *strn, char *sep);
