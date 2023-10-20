11a2fe26c1708e41fce45a364d4bbdab51f10f56
dhafee <divineekoh@gmail.com>
2023-10-20 07:15:16 -0700

    simple_shell

---
+++ exec.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * execute - Execute a command from a specified path in a child process.
+ *
+ * @args: An array of  arguments for the command.
+ *
+ * Return: The exist status of execute command.
+ */
+
+int exec(char **args)
+{
+	int id = fork(), status;
+
+	if (id == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+			perror("Error");
+	}
+	else
+	{
+		wait(&status);
+		if (WIFEXITED(status);
+	}
+
+	return (status);
