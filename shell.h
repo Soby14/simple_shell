dd94236bc05616a5ed0c3c8241c726ecb19decf9
Soby14 <sobyria@gmail.com>
2023-10-20 07:08:04 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -11,7 +11,7 @@
 
 /*PROTOTYPES*/
 int _strnlen(char *s);
-int execute(char **args);
+int exec(char **args);
 char *_strncpy(char *dest, char *src);
 void _env(void);
 char *_strncat(char *dest, char *src);
