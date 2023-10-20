809dc500e0ede5a20b26303272ae0ea8a37a83bb
dhafee <divineekoh@gmail.com>
2023-10-20 08:47:35 -0700

    simple_shell

---
+++ env.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* _getenv - Retrieve the values of an environment variable.
+*
+* @env_var: The name of the environment variable to retrieve.
+*
+* Return: A pointer to the content of the environment var, or NULL if not seen.
+*/
+char *_getenv(char *env_var)
+{
+int i = 0, j;
+int status;
+while (environ[i])
+{
+status = 1;
+for (j = 0; environ[i][j] != '='; j++)
+{
+if (environ[i][j] != env_var[j])
+status = 0;
+}
+if (status == 1)
+break;
+i++;
+}
+return (&environ[i][j + 1]);
+}
+/**
+* _env - prints out environment
+*/
+void _env(void)
+{
+int i = 0;
+while (environ[i])
+{
+printf("%s\n", environ[i]);
+i++;
+}
+}
